86971ccf0e9ffe52b8279ae3d626c9cca82d97a3
ThanatosJun <122719731+ThanatosJun@users.noreply.github.com>
2023-12-26 20:33:50 +0800

    Member Helper add dopost

--- NCU_Campus/WEB-INF/classes/ncu/im3069/demo/controller/MemberController.java
+++ NCU_Campus/WEB-INF/classes/ncu/im3069/demo/controller/MemberController.java
@@ -32,6 +32,54 @@ public class MemberController extends HttpServlet {
      * @throws ServletException the servlet exception
      * @throws IOException Signals that an I/O exception has occurred.
      */
+    
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            /** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
+            JsonReader jsr = new JsonReader(request);
+            JSONObject jso = jsr.getObject();
+            
+            /** 取出經解析到JSONObject之Request參數 */
+            String first_name = jso.getString("first_name");
+            String last_name = jso.getString("last_name");
+            String birthday = jso.getString("birthday");
+            String email = jso.getString("email");
+            String password = jso.getString("password");
+            String user_name = jso.getString("name");
+            String phone = jso.getString("phone");
+            
+            /** 建立一個新的會員物件 */
+            Member m = new Member(first_name,last_name,birthday,email, password, user_name,phone);
+            
+            /** 後端檢查是否有欄位為空值，若有則回傳錯誤訊息 */
+            if(email.isEmpty() || password.isEmpty() || name.isEmpty()) {
+                /** 以字串組出JSON格式之資料 */
+                String resp = "{\"status\": \'400\', \"message\": \'欄位不能有空值\', \'response\': \'\'}";
+                /** 透過JsonReader物件回傳到前端（以字串方式） */
+                jsr.response(resp, response);
+            }
+            /** 透過MemberHelper物件的checkDuplicate()檢查該會員電子郵件信箱是否有重複 */
+            else if (!mh.checkDuplicate(m)) {
+                /** 透過MemberHelper物件的create()方法新建一個會員至資料庫 */
+                JSONObject data = mh.create(m);
+                
+                /** 新建一個JSONObject用於將回傳之資料進行封裝 */
+                JSONObject resp = new JSONObject();
+                resp.put("status", "200");
+                resp.put("message", "成功! 註冊會員資料...");
+                resp.put("response", data);
+                
+                /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+                jsr.response(resp, response);
+            }
+            else {
+                /** 以字串組出JSON格式之資料 */
+                String resp = "{\"status\": \'400\', \"message\": \'新增帳號失敗，此E-Mail帳號重複！\', \'response\': \'\'}";
+                /** 透過JsonReader物件回傳到前端（以字串方式） */
+                jsr.response(resp, response);
+            }
+        }
+    
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
         JsonReader jsr = new JsonReader(request);
@@ -124,15 +172,13 @@ public class MemberController extends HttpServlet {
         JsonReader jsr = new JsonReader(request);
         JSONObject jso = jsr.getObject();
         int ID = jso.getInt("ID");
-        String Authority = jso.getString("Authority");
         String FirstName = jso.getString("First_Name");
         String LastName = jso.getString("Last_Name");
         String Birthday = jso.getString("Birthday");
         String email = jso.getString("email");
         String Phone = jso.getString("Phone");
         String UserName = jso.getString("User_Name");
-        String Password = jso.getString("Password");
-        Member m = new Member(ID, Authority, FirstName, LastName, Birthday, email, Phone, UserName, Password);
+        Member m = new Member(ID,FirstName,LastName,Birthday,email,Phone,UserName);
         JSONObject data = m.update();
         JSONObject resp = new JSONObject();
         resp.put("status", "200");
