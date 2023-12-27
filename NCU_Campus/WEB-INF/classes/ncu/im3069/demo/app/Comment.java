1e43c8bc00d1d0f1d6be7c0748f28f92b304a9d4
TiasheKnight <115419422+TiasheKnight@users.noreply.github.com>
2023-12-28 05:32:26 +0800

    comment helper

--- NCU_Campus/WEB-INF/classes/ncu/im3069/demo/app/Comment.java
+++ NCU_Campus/WEB-INF/classes/ncu/im3069/demo/app/Comment.java
@@ -7,14 +7,15 @@ public class Comment {
     private int ID;
     private int User_ID;
     private int Activity_ID;
-    private String Comment;
+    private String Content;
     private String TimeStamp;
     private CommentHelper CH = CommentHelper.getHelper();
 
-    public Comment(int Activity_ID, int User_ID, String Comment, String TimeStamp) {
-        this.Activity_ID = Activity_ID;
+    public Comment(int ID, int Activity_ID, int User_ID, String Content, String TimeStamp) {
+    	this.ID = ID;
+    	this.Activity_ID = Activity_ID;
         this.User_ID = User_ID;
-        this.Comment = Comment;
+        this.Content = Content;
         this.TimeStamp = TimeStamp;
     }
 
@@ -30,8 +31,8 @@ public class Comment {
         return this.Activity_ID;
     }
 
-    public String getComment() {
-        return this.Comment;
+    public String getContent() {
+        return this.Content;
     }
 
     public String getTimeStamp() {
@@ -43,8 +44,8 @@ public class Comment {
         jso.put("id", getID());
         jso.put("user_id", getUser_ID());
         jso.put("activity_id", getActivity_ID());
-        jso.put("comment", getComment());
-        jso.put("timestamp", getTimeStamp());
+        jso.put("content", getContent());
+        jso.put("time", getTimeStamp());
         return jso;
     }
 
--- NCU_Campus/WEB-INF/classes/ncu/im3069/demo/app/CommentHelper.java
+++ NCU_Campus/WEB-INF/classes/ncu/im3069/demo/app/CommentHelper.java
@@ -2,6 +2,9 @@ package ncu.im3069.demo.app;
 
 import java.sql.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.json.*;
 
 import ncu.im3069.demo.util.DBMgr;
@@ -49,5 +52,322 @@ public class CommentHelper {
 
         return ch;
     }
-    
+    public JSONObject getAll() {
+    	Comment c = null;
+        JSONArray jsa = new JSONArray();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+        ResultSet rs = null;
+
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "SELECT * FROM `campus`.`Comment`";
+
+            /** 將參數回填至SQL指令當中，若無則不用只需要執行 prepareStatement */
+            pres = conn.prepareStatement(sql);
+            /** 執行查詢之SQL指令並記錄其回傳之資料 */
+            rs = pres.executeQuery();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+
+            /** 透過 while 迴圈移動pointer，取得每一筆回傳資料 */
+            while(rs.next()) {
+                /** 每執行一次迴圈表示有一筆資料 */
+                row += 1;
+
+                /** 將 ResultSet 之資料取出 */
+                int id = rs.getInt("id");
+                int user_id = rs.getInt("user_id");
+                int activity_id = rs.getInt("activity_id");
+                String content=rs.getString("content");
+                String timestamp=rs.getString("timestamp");
+
+
+                /** 將每一筆商品資料產生一名新Product物件 */
+                c = new Comment(id, user_id, activity_id,content,timestamp);
+                /** 取出該項商品之資料並封裝至 JSONsonArray 內 */
+                jsa.put(c.getData());
+            }
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(rs, pres, conn);
+        }
+
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+
+        /** 將SQL指令、花費時間、影響行數與所有會員資料之JSONArray，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("row", row);
+        response.put("time", duration);
+        response.put("data", jsa);
+
+        return response;
+    }
+    public JSONObject getById(String Comment_id) {
+    	Comment c = null;
+    	/** 用於儲存所有檢索回之會員，以JSONArray方式儲存 */
+        JSONArray jsa = new JSONArray();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+        ResultSet rs = null;
+
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "SELECT * FROM `campus`.`Comment` WHERE `Comment`.`id` = ?";
+
+            /** 將參數回填至SQL指令當中，若無則不用只需要執行 prepareStatement */
+            pres = conn.prepareStatement(sql);
+            pres.setString(1, Comment_id);
+            /** 執行查詢之SQL指令並記錄其回傳之資料 */
+            rs = pres.executeQuery();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+
+            /** 透過 while 迴圈移動pointer，取得每一筆回傳資料 */
+            while(rs.next()) {
+                /** 將 ResultSet 之資料取出 */
+            	row += 1;
+            	int id = rs.getInt("id");
+                int user_id = rs.getInt("user_id");
+                int activity_id = rs.getInt("activity_id");
+                String content=rs.getString("content");
+                String timestamp=rs.getString("timestamp");
+
+                /** 將每一筆商品資料產生一名新Product物件 */
+                c = new Comment(id, user_id, activity_id,content,timestamp);
+                jsa.put(c.getData());
+            }
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(rs, pres, conn);
+        }
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+
+        /** 將SQL指令、花費時間、影響行數與所有會員資料之JSONArray，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("row", row);
+        response.put("time", duration);
+        response.put("data", jsa);
+
+        return response;
+    }
+ 
+    public JSONObject create(Comment Comment) {
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "INSERT INTO `campus`.`Comment`(`id`, `user_id`, `activity_id`,`content`,`timestamp`)"
+                    + " VALUES(?, ?, ?, ?, ?)";
+            
+            /** 取得所需之參數 */
+            int ID = Comment.getID();
+            int user_id = Comment.getUser_ID();
+            int activity_id = Comment.getActivity_ID();
+            String content=Comment.getContent();
+            String timestamp=Comment.getTimeStamp();
+
+            
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setInt(1, ID);
+            pres.setInt(2, user_id);
+            pres.setInt(3, activity_id);
+            pres.setString(4, content);
+            pres.setString(5, timestamp);
+            
+            /** 執行新增之SQL指令並記錄影響之行數 */
+            row = pres.executeUpdate();
+            
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+            
+            
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(pres, conn);
+        }
+
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+
+        /** 將SQL指令、花費時間與影響行數，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("time", duration);
+        response.put("row", row);
+
+        return response;
+    }
+    public JSONArray createByList(long Comment_ID, List<Comment> Comment) {
+        JSONArray jsa = new JSONArray();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+
+        for(int i=0 ; i < Comment.size() ; i++) {
+        	Comment m = Comment.get(i);
+
+            /** 取得所需之參數 */
+        	int ID = m.getID();
+            int user_id = m.getUser_ID();
+            int activity_id = m.getActivity_ID();
+            String content=m.getContent();
+            String timestamp=m.getContent();
+
+            try {
+                /** 取得資料庫之連線 */
+                conn = DBMgr.getConnection();
+                /** SQL指令 */
+                String sql = "INSERT INTO `campus`.`Comment`(`id`, `user_id`, `activity_id`,`content`,`timestamp`)"
+                        + " VALUES(?, ?, ?)";
+
+                /** 將參數回填至SQL指令當中 */
+                pres = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                pres.setInt(1, ID);
+                pres.setInt(2, user_id);
+                pres.setInt(3, activity_id);
+                pres.setString(4, content);
+                pres.setString(5, timestamp);
+
+
+                /** 執行新增之SQL指令並記錄影響之行數 */
+                pres.executeUpdate();
+
+                /** 紀錄真實執行的SQL指令，並印出 **/
+                exexcute_sql = pres.toString();
+                System.out.println(exexcute_sql);
+
+                ResultSet rs = pres.getGeneratedKeys();
+
+                if (rs.next()) {
+                    int id = rs.getInt(1);
+                    jsa.put(id);
+                }
+            } catch (SQLException e) {
+                /** 印出JDBC SQL指令錯誤 **/
+                System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+            } catch (Exception e) {
+                /** 若錯誤則印出錯誤訊息 */
+                e.printStackTrace();
+            } finally {
+                /** 關閉連線並釋放所有資料庫相關之資源 **/
+                DBMgr.close(pres, conn);
+            }
+        }
+
+        return jsa;
+    }
+    public ArrayList<Comment> getCommentByMemberId(int member_id) {
+        ArrayList<Comment> result = new ArrayList<Comment>();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        ResultSet rs = null;
+        Comment m;
+
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "SELECT * FROM `campus`.`Comment` WHERE `Comment`.`id` = ?";
+
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setInt(1, member_id);
+
+            /** 執行新增之SQL指令並記錄影響之行數 */
+            rs = pres.executeQuery();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+
+            while(rs.next()) {
+                /** 每執行一次迴圈表示有一筆資料 */
+
+                /** 將 ResultSet 之資料取出 */
+                int  ID = rs.getInt("id");
+                int  user_id = rs.getInt("user_id");
+                int  activity_id = rs.getInt("activity_id");
+                String content=rs.getString("content");
+                String timestamp=rs.getString("timestamp");
+
+
+                /** 將參數回填至SQL指令當中 */
+
+                /** 將每一筆會員資料產生一名新Member物件 */
+                m = new Comment(ID, user_id, activity_id,content,timestamp);
+                /** 取出該名會員之資料並封裝至 JSONsonArray 內 */
+                result.add(m);
+            }
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(pres, conn);
+        }
+
+        return result;
+    }
+
+
 }
