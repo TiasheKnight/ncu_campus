0c322d7fd28ab0478d8a20c66b00104fdbb0bf90
pizzaInmystomach <134912847+pizzaInmystomach@users.noreply.github.com>
2023-12-28 02:35:29 +0800

    Update create_table-SA_database_item.sql

--- NCU_Campus/SQL/create_table-SA_database_item.sql
+++ NCU_Campus/SQL/create_table-SA_database_item.sql
@@ -1,5 +1,5 @@
-drop table if exists sa_database_item;
-CREATE TABLE sa_database_item (
+drop table if exists item;
+CREATE TABLE item (
 	id int,
 	item_name text,
 	item_price int,
@@ -7,7 +7,7 @@ CREATE TABLE sa_database_item (
 	img int
 );
 
-INSERT INTO sa_database_item VALUES (1,'呼喚的號角',15,'可以兌換乙次發起緊急活動',NULL);
-INSERT INTO sa_database_item VALUES (2,'竹蜻蜓',20,'可以兌換貳次發起緊急活動',NULL);
-INSERT INTO sa_database_item VALUES (3,'翻譯蒟蒻',25,'可以兌換參次發起緊急活動',NULL);
-INSERT INTO sa_database_item VALUES (4,'想像袋',30,'可以兌換肆次發起緊急活動',NULL);
+INSERT INTO item VALUES (1,'呼喚的號角',15,'可以兌換乙次發起緊急活動',NULL);
+INSERT INTO item VALUES (2,'竹蜻蜓',20,'可以兌換貳次發起緊急活動',NULL);
+INSERT INTO item VALUES (3,'翻譯蒟蒻',25,'可以兌換參次發起緊急活動',NULL);
+INSERT INTO item VALUES (4,'想像袋',30,'可以兌換肆次發起緊急活動',NULL);
