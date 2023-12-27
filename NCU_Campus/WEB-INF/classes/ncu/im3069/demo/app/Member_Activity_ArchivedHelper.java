bb420f5526c1bfa4ac90c98283f15b4f07ed8593
ying0607 <chingying0607@gmail.com>
2023-12-28 04:30:18 +0800

    member activity archived helper update

--- NCU_Campus/WEB-INF/classes/ncu/im3069/demo/app/Member_Activity_ArchivedHelper.java
+++ NCU_Campus/WEB-INF/classes/ncu/im3069/demo/app/Member_Activity_ArchivedHelper.java
@@ -2,6 +2,9 @@ package ncu.im3069.demo.app;
 
 import java.sql.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.json.*;
 
 import ncu.im3069.demo.util.DBMgr;
@@ -50,5 +53,107 @@ public class Member_Activity_ArchivedHelper {
         return maah;
     }
 
+    public JSONArray createByList(long activtiy_id, List<Member_Activity_Archived> memberactivityarchived) {
+        JSONArray jsa = new JSONArray();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
 
+        for(int i=0 ; i < memberactivityarchived.size() ; i++) {
+            Member_Activity_Archived maa = memberactivityarchived.get(i);
+
+            /** 取得所需之參數 */
+            int maa_id = maa.getID();
+            int activity_id = maa.getActivity_ID();
+            String user_id = maa.getUser_ID();
+
+            try {
+                /** 取得資料庫之連線 */
+                conn = DBMgr.getConnection();
+                /** SQL指令 */
+                String sql = "INSERT INTO `campus`.`member_activty_archived`(`id`, `activity_id`, `user_id`)"
+                        + " VALUES(?, ?, ?)";
+
+                /** 將參數回填至SQL指令當中 */
+                pres = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                pres.setInt(1, maa_id);
+                pres.setInt(2, activity_id);
+                pres.setString(3, user_id);
+
+                /** 執行新增之SQL指令並記錄影響之行數 */
+                pres.executeUpdate();
+
+                /** 紀錄真實執行的SQL指令，並印出 **/
+                exexcute_sql = pres.toString();
+                System.out.println(exexcute_sql);
+
+                ResultSet rs = pres.getGeneratedKeys();
+
+                if (rs.next()) {
+                    long id = rs.getLong(1);
+                    jsa.put(id);
+                }
+            } catch (SQLException e) {
+                /** 印出JDBC SQL指令錯誤 **/
+                System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+            } catch (Exception e) {
+                /** 若錯誤則印出錯誤訊息 */
+                e.printStackTrace();
+            } finally {
+                /** 關閉連線並釋放所有資料庫相關之資源 **/
+                DBMgr.close(pres, conn);
+            }
+        }
+
+        return jsa;
+    }
+
+    public ArrayList<Member_Activity_Archived> getMemberActivityArchived(int member_id) {
+        ArrayList<Member_Activity_Archived> result = new ArrayList<Member_Activity_Archived>();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        ResultSet rs = null;
+        Member_Activity_Archived maa;
+
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "SELECT * FROM `campus`.`member_activity_archived` WHERE `member_activity_archived`.`user_id` = ?";
+
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setInt(1, member_id);
+
+            /** 執行新增之SQL指令並記錄影響之行數 */
+            rs = pres.executeQuery();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+
+            while(rs.next()) {
+                /** 每執行一次迴圈表示有一筆資料 */
+
+                /** 將 ResultSet 之資料取出 */
+                String user_id = rs.getString("user_id");
+                int activity_id = rs.getInt("activity_id");
+
+                /** 將每一筆會員資料產生一名新Member_Activity_Archived物件 */
+                maa = new Member_Activity_Archived(activity_id, user_id);
+                /** 取出該名會員之資料並封裝至 JSONsonArray 內 */
+                result.add(maa);
+            }
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(pres, conn);
+        }
+
+        return result;
+    }
 }
