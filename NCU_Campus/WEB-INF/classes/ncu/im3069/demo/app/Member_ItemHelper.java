64608bea43937b1083e9b764925eaf164b954fd6
TiasheKnight <115419422+TiasheKnight@users.noreply.github.com>
2023-12-28 04:48:45 +0800

    Update Member_ItemHelper.java

--- NCU_Campus/WEB-INF/classes/ncu/im3069/demo/app/Member_ItemHelper.java
+++ NCU_Campus/WEB-INF/classes/ncu/im3069/demo/app/Member_ItemHelper.java
@@ -2,6 +2,9 @@ package ncu.im3069.demo.app;
 
 import java.sql.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.json.*;
 
 import ncu.im3069.demo.util.DBMgr;
@@ -50,6 +53,421 @@ public class Member_ItemHelper {
         return mih;
     }
 
+    public JSONObject getAll() {
+    	Member_Item mi = null;
+        JSONArray jsa = new JSONArray();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+        ResultSet rs = null;
+
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "SELECT * FROM `campus`.`Member_Item`";
+
+            /** 將參數回填至SQL指令當中，若無則不用只需要執行 prepareStatement */
+            pres = conn.prepareStatement(sql);
+            /** 執行查詢之SQL指令並記錄其回傳之資料 */
+            rs = pres.executeQuery();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+
+            /** 透過 while 迴圈移動pointer，取得每一筆回傳資料 */
+            while(rs.next()) {
+                /** 每執行一次迴圈表示有一筆資料 */
+                row += 1;
+
+                /** 將 ResultSet 之資料取出 */
+                int id = rs.getInt("id");
+                int user_id = rs.getInt("user_id");
+                int item_id = rs.getInt("item_id");
+                int item_quantity = rs.getInt("item_quantity");
 
 
+                /** 將每一筆商品資料產生一名新Product物件 */
+                mi = new Member_Item(id, user_id, item_id,item_quantity);
+                /** 取出該項商品之資料並封裝至 JSONsonArray 內 */
+                jsa.put(mi.getData());
+            }
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(rs, pres, conn);
+        }
+
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+
+        /** 將SQL指令、花費時間、影響行數與所有會員資料之JSONArray，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("row", row);
+        response.put("time", duration);
+        response.put("data", jsa);
+
+        return response;
+    }
+    public JSONObject getById(String Member_Item_id) {
+    	Member_Item mi = null;
+    	/** 用於儲存所有檢索回之會員，以JSONArray方式儲存 */
+        JSONArray jsa = new JSONArray();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+        ResultSet rs = null;
+
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "SELECT * FROM `campus`.`Member_Item` WHERE `Member_Item`.`id` = ?";
+
+            /** 將參數回填至SQL指令當中，若無則不用只需要執行 prepareStatement */
+            pres = conn.prepareStatement(sql);
+            pres.setString(1, Member_Item_id);
+            /** 執行查詢之SQL指令並記錄其回傳之資料 */
+            rs = pres.executeQuery();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+
+            /** 透過 while 迴圈移動pointer，取得每一筆回傳資料 */
+            while(rs.next()) {
+                /** 將 ResultSet 之資料取出 */
+            	row += 1;
+            	int id = rs.getInt("id");
+                int user_id = rs.getInt("user_id");
+                int item_id = rs.getInt("item_id");
+                int item_quantity = rs.getInt("item_quantity");
+                /** 將每一筆商品資料產生一名新Product物件 */
+                mi = new Member_Item(id, user_id, item_id,item_quantity);
+                jsa.put(mi.getData());
+            }
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(rs, pres, conn);
+        }
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+
+        /** 將SQL指令、花費時間、影響行數與所有會員資料之JSONArray，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("row", row);
+        response.put("time", duration);
+        response.put("data", jsa);
+
+        return response;
+    }
+
+    public JSONObject create(Member_Item member_item) {
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "INSERT INTO `campus`.`Member_Item`(`id`, `user_id`, `item_id`, `item_quantity`)"
+                    + " VALUES(?, ?, ?,?)";
+
+            /** 取得所需之參數 */
+            int ID = member_item.getID();
+            int user_id = member_item.getUser_ID();
+            int item_id = member_item.getItem_ID();
+            int item_quantity = member_item.getItem_Quantity();
+
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setInt(1, ID);
+            pres.setInt(2, user_id);
+            pres.setInt(3, item_id);
+            pres.setInt(3, item_quantity);
+
+
+            /** 執行新增之SQL指令並記錄影響之行數 */
+            row = pres.executeUpdate();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(pres, conn);
+        }
+
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+
+        /** 將SQL指令、花費時間與影響行數，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("time", duration);
+        response.put("row", row);
+
+        return response;
+    }
+    public JSONObject deleteByID(int id) {
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+        ResultSet rs = null;
+
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+
+            /** SQL指令 */
+            String sql = "DELETE FROM `campus`.`Member_Item` WHERE `id` = ? LIMIT 1";
+
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setInt(1, id);
+            /** 執行刪除之SQL指令並記錄影響之行數 */
+            row = pres.executeUpdate();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(rs, pres, conn);
+        }
+
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+
+        /** 將SQL指令、花費時間與影響行數，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("row", row);
+        response.put("time", duration);
+
+        return response;
+    }
+    public JSONObject update(Member_Item r) {
+        /** 紀錄回傳之資料 */
+        JSONArray jsa = new JSONArray();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "Update `campus`.`Member_Item` SET `id` = ? ,`user_id` = ? , `item_id`, `item_quantity` = ? WHERE `id` = ?";
+            /** 取得所需之參數 */
+            int  ID = r.getID();
+            int  user_id = r.getUser_ID();
+            int  item_id = r.getItem_ID();
+            int  item_quantity = r.getItem_Quantity();
+
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setInt(1, ID);
+            pres.setInt(2, user_id);
+            pres.setInt(3, item_id);
+            pres.setInt(4, item_quantity);
+            /** 執行更新之SQL指令並記錄影響之行數 */
+            row = pres.executeUpdate();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(pres, conn);
+        }
+
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+
+        /** 將SQL指令、花費時間與影響行數，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("row", row);
+        response.put("time", duration);
+        response.put("data", jsa);
+
+        return response;
+    }
+    public JSONArray createByList(long Member_Item_ID, List<Member_Item> member_item) {
+        JSONArray jsa = new JSONArray();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+
+        for(int i=0 ; i < member_item.size() ; i++) {
+        	Member_Item mi = member_item.get(i);
+
+            /** 取得所需之參數 */
+            int  ID = mi.getID();
+            int  user_id = mi.getUser_ID();
+            int  item_id = mi.getItem_ID();
+            int  item_quantity = mi.getItem_Quantity();
+
+            try {
+                /** 取得資料庫之連線 */
+                conn = DBMgr.getConnection();
+                /** SQL指令 */
+                String sql = "INSERT INTO `campus`.`Member_Item`(`id`, `user_id`, `item_id`, `item_quantity)"
+                        + " VALUES(?, ?, ?,?)";
+
+                /** 將參數回填至SQL指令當中 */
+                pres = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                pres.setInt(1, ID);
+                pres.setInt(2, user_id);
+                pres.setInt(3, item_id);
+                pres.setInt(3, item_quantity);
+
+
+                /** 執行新增之SQL指令並記錄影響之行數 */
+                pres.executeUpdate();
+
+                /** 紀錄真實執行的SQL指令，並印出 **/
+                exexcute_sql = pres.toString();
+                System.out.println(exexcute_sql);
+
+                ResultSet rs = pres.getGeneratedKeys();
+
+                if (rs.next()) {
+                    int id = rs.getInt(1);
+                    jsa.put(id);
+                }
+            } catch (SQLException e) {
+                /** 印出JDBC SQL指令錯誤 **/
+                System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+            } catch (Exception e) {
+                /** 若錯誤則印出錯誤訊息 */
+                e.printStackTrace();
+            } finally {
+                /** 關閉連線並釋放所有資料庫相關之資源 **/
+                DBMgr.close(pres, conn);
+            }
+        }
+
+        return jsa;
+    }
+    public ArrayList<Member_Item> getMember_ItemByMemberId(int member_id) {
+        ArrayList<Member_Item> result = new ArrayList<Member_Item>();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        ResultSet rs = null;
+        Member_Item mi;
+
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "SELECT * FROM `campus`.`Member_Item` WHERE `Member_Item`.`id` = ?";
+
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setInt(1, member_id);
+
+            /** 執行新增之SQL指令並記錄影響之行數 */
+            rs = pres.executeQuery();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+
+            while(rs.next()) {
+                /** 每執行一次迴圈表示有一筆資料 */
+
+                /** 將 ResultSet 之資料取出 */
+                int  ID = rs.getInt("id");
+                int  user_id = rs.getInt("user_id");
+                int  item_id = rs.getInt("item_id");
+                int  item_quantity = rs.getInt("item_quantity");
+
+                /** 將參數回填至SQL指令當中 */
+
+                /** 將每一筆會員資料產生一名新Member物件 */
+                mi = new Member_Item(ID, user_id, item_id,item_quantity);
+                /** 取出該名會員之資料並封裝至 JSONsonArray 內 */
+                result.add(mi);
+            }
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(pres, conn);
+        }
+
+        return result;
+    }
 }
